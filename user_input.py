1954b247e48abbc547e1a60eaf9e9c297923579c
Lindt7 <125860169+csikalieh@users.noreply.github.com>
2024-04-01 13:49:06 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,21 @@
+# Ask the user for their name
+name = input("Please enter your name: ")
+
+# Print a greeting with the user's name
+print("Hello, " + name + "! Nice to meet you!")
+
+# Ask the user for their age
+age = input("Please enter your age: ")
+
+# Print a message with the user's age
+print("You entered: " + age + " years old.")
+
+# Ask the user for their location
+location = input("Please enter your location: ")
+
+# Print a message with the user's location
+print("You entered: " + location)
+
+# Print a personalized message
+print("Hello, " + name + "! It's great to know that you're " + age + " years old and located in " + location + ". Welcome!")
+
